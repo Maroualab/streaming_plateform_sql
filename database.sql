7f9596f60f14c8d387bae4dcd10ff024140a3d38
Labballi Maroua <labballimaroua@gmail.com>
2024-12-09 20:26:43 +0100

    create table user

--- database.sql
+++ database.sql
@@ -3,3 +3,17 @@
 
 --create database
 CREATE DATABASE streaming_plateform;
+
+--use database
+USE streaming_plateform;
+
+--add table user_plt
+CREATE TABLE user_plt(
+UserID int PRIMARY KEY, 
+FirstName varchar (100) NOT NULL,
+LastName varchar (100) NOT NULL,
+Email varchar (100) UNIQUE,
+RegistrationDate date,
+SubscriptionID int NOT NULL 
+)
+
